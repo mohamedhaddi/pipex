3f406931778c33b61f3699dbca38a3ce81eb4ca9
Mohamed Haddi <mhaddi@student.1337.ma>
2021-06-05 17:34:34 +0100

    implement input redirection

--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 /*   By: mhaddi <mhaddi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 16:50:38 by mhaddi            #+#    #+#             */
-/*   Updated: 2021/06/05 14:05:55 by mhaddi           ###   ########.fr       */
+/*   Updated: 2021/06/05 17:32:53 by mhaddi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/fcntl.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -20,43 +21,58 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	close(0); // release fd 0 so that it no longer refers to any file and may be reused
 	(void)argc;
+	int pipe_fds[2];
+
+	// TO-DO: ERROR MANAGEMENT
 	/*
 	if (argc != 5)
 	{
-		perror("Error\nThere should be five arguments:\n./pipex file1 cmd1 cmd2 file2");
+		perror("Error\nThere should be five arguments:\n./pipex file1 first_cmd cmd2 file2");
 		exit(EXIT_FAILURE);
 	}
 	*/
-	open(argv[1], 'r'); // open file with fd 0
 
+	// RELASE stdin AND OPEN ARGV[1]
+	close(0); // release fd 0 so that it no longer refers to any file and may be reused
+	open(argv[1], O_RDONLY); // open file with fd 0
+
+	// FIND PATH AND IMPLEMENT INPUT REDIRECTION TO CMD1
 	// child process
-	if (fork() == 0)
-	{
-		char **cmd1 = ft_split(argv[2], ' ');
-		// HOW TO GET THE ABSOLUTE PATH
-		int i = 0;
-		while (envp[i]) {
-			char **var = ft_split(envp[i], '=');
-			if (ft_strncmp("PATH", var[0], 5) == 0) {
-				printf("%s\n", envp[i]);
-				char **paths = ft_split(var[1], ':');
-				int j = 0;
-				int fd = -1;
-				while (paths[j] && fd == -1) {
-					fd = open(ft_strjoin(paths[j], ft_strjoin("/", cmd1[0])), O_RDONLY);
-					if (fd != -1)
-						break ;
-					j++;
-				}
-				if (fd != -1)
-					printf("Found it! --> %s\n", paths[j]);
-				close(fd);
+	//int first_pid = fork();
+	//if (first_pid == 0)
+	//{
+	char **first_cmd = ft_split(argv[2], ' ');
+	int i = 0;
+	while (envp[i]) {
+		char **var = ft_split(envp[i], '=');
+		if (ft_strncmp("PATH", var[0], 5) == 0) {
+			char **paths = ft_split(var[1], ':');
+			int j = 0;
+			int path_fd = -1;
+			char *full_path;
+			while (paths[j] && path_fd == -1) {
+				full_path = ft_strjoin(paths[j], ft_strjoin("/", first_cmd[0]));
+				path_fd = open(full_path, O_RDONLY);
+				if (path_fd != -1)
+					break ;
+				j++;
 			}
-			i++;
+			if (path_fd != -1 && full_path)
+			{
+				close(path_fd);
+				//first_cmd[0] = full_path;
+				execve(full_path, first_cmd, envp); // by default, programs reads from stdin (path_fd 0)
+			}
 		}
-		//execvp(cmd1, cmd1);
-		//execve(cmd1[0], cmd1, envp); // by default, programs reads from stdin (fd 0)
+		i++;
 	}
+	//}
+	//waitpid(first_pid, NULL, 0);
+	
+	// CREATE A PIPE
+	pipe(pipe_fds);
+
+
+	return (0);
 }
